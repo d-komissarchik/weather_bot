920f8eed806b99518418b050b191c947af3b8b51
d-komissarchik <0667669609@ukr.net>
2023-11-04 16:15:50 +0200

    added database

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import telebot
 import requests
 import json
-from config import geo_key
+from config import api_key
 
-bot = telebot.TeleBot()
-API = geo_key
+bot = telebot.TeleBot('5356962954:AAGpPLeRTCf5ZEknWMeQlv87y8DGR86YdFY')
+API = api_key
 
 
 @bot.message_handler(commands=['start'])
@@ -12,21 +12,22 @@ def start(message):
     bot.send_message(message.chat.id, 'Hello. Enter the name of the city: ')
 
 
-#декоратором проверяем что пользователь отправил имено текст, если да, то срабатывает ф-ия
+# декоратором проверяем что пользователь отправил имено текст, если да, то срабатывает ф-ия
 @bot.message_handler(content_types=['text'])
 def get_weather(message):
-    city = message.text.strip().lower() #удаляем пробелы и переводим в нижний регистр
+    city = message.text.strip().lower()  # удаляем пробелы и переводим в нижний регистр
     show_city = city.title()
     res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
-    if res.status_code == 200:#проверяем введен ли правильно город
-        data = json.loads(res.text)#получаем текст с джейсон и теперь можем добраться до его атрибутов(main, temp)
+    if res.status_code == 200:  # проверяем введен ли правильно город
+        data = json.loads(res.text)  # получаем текст с джейсон и теперь можем добраться до его атрибутов(main, temp)
         temp = data["main"]["temp"]
         bot.reply_to(message, f'Now temperature in {show_city}: {temp}°')
-        #переменная хранит разное изображение в зависимости от текущей погоды
+        # переменная хранит разное изображение в зависимости от текущей погоды
         image = 'sunny.png' if temp > 5.0 else 'clouds.png'
         file = open('./' + image, 'rb')
         bot.send_photo(message.chat.id, file)
     else:
         bot.reply_to(message, f'Incorrect name of  city - {show_city}')
+
 
 bot.polling(none_stop=True)
