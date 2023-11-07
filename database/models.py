bce3c80441ecda596c769604bd36190db06f1d75
d-komissarchik <0667669609@ukr.net>
2023-11-07 16:51:39 +0200

    added tables User, Reviews

--- database/models.py
+++ database/models.py
@@ -7,10 +7,31 @@ Base = declarative_base()
 
 class Book(Base):
     __tablename__ = 'books'
-    id = Column(String(60), nullable=False)
+    id = Column(Integer, primary_key=True)
     title = Column(String(30), nullable=False)
     author = Column(String(30), nullable=False)
-    rewiews = relationship('Reviews', backref='book', lazy=True)
+    reviews = relationship('Reviews', backref='book', lazy=True)
 
     def __repr__(self):
         return self.title
+
+
+class Reviews(Base):
+    __tablename__ = 'reviews'
+    id = Column(Integer, primary_key=True)
+    text = Column(String(2000), nullable=False)
+    book_id = Column(Integer, ForeignKey('book.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self):
+        return f'Вiд {self.reviewer}'
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20), nullable=False)
+    reviews = relationship('Reviews', backref='reviewer', lazy=True)
+
+    def __repr__(self):
+        return self.name
