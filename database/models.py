7e0d222954174adde753ccd19c7d6b00f5d39218
d-komissarchik <0667669609@ukr.net>
2023-11-07 16:29:22 +0200

    added table

--- database/models.py
+++ database/models.py
@@ -1,3 +1,16 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class Book(Base):
+    __tablename__ = 'books'
+    id = Column(String(60), nullable=False)
+    title = Column(String(30), nullable=False)
+    author = Column(String(30), nullable=False)
+    rewiews = relationship('Reviews', backref='book', lazy=True)
+
+    def __repr__(self):
+        return self.title
